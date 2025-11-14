e9d8153ce10097aab8825649e1831ee937a35cfa
R-Islombek <islombekraximjanov97dev@gmail.com>
2025-11-14 15:23:45 +0500

    footer qismini boshladim

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-
+import { useTranslation } from "react-i18next";
 const Footer = () => {
+  const { t } = useTranslation();
   return (
     <div>
             <h1>Footer Page</h1>
