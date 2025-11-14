510b4e06ad6e60ff686a98967929e1a4ac4972a1
R-Islombek <islombekraximjanov97dev@gmail.com>
2025-11-14 15:21:25 +0500

    Responsivelik qildim yana

--- src/components/Latests/Latests.jsx
+++ src/components/Latests/Latests.jsx
@@ -7,27 +7,25 @@ import { useTranslation } from "react-i18next";
 
 const Latests = () => {
     const { t } = useTranslation();
+    const data = [
+        { img: blooth, title: t("latest.title"), text: t("latest.text") },
+        { img: dend, title: t("latest.title1"), text: t("latest.text1") },
+        { img: sambufer, title: t("latest.title2"), text: t("latest.text2") },
+    ];
+
     return (
         <div className='Latest'>
             <div className='container'>
                 <div className='latest__content'>
                     <h4 className='latest__heading'>{t("latest.heading")}</h4>
                     <ul className='latest__list'>
-                        <li className='latest__item'>
-                            <img src={blooth} alt="Blooth" />
-                            <h4 className='latest__title'>{t("latest.title")}</h4>
-                            <p className='latest__text'>{t("latest.text")}</p>
-                        </li>
-                        <li>
-                            <img src={dend} alt="Dend" />
-                            <h4 className='latest__title'>{t("latest.title1")}</h4>
-                            <p className='latest__text'>{t("latest.text1")}</p>
-                        </li>
-                        <li>
-                            <img src={sambufer} alt="Sambufer" />
-                            <h4 className='latest__title'>{t("latest.title2")}</h4>
-                            <p className='latest__text'>{t("latest.text2")}</p>
-                        </li>
+                        {data.map((item, index) => (
+                            <li key={index} className='latest__item'>
+                                <img src={item.img} alt={item.title} />
+                                <h4 className='latest__title'>{item.title}</h4>
+                                <p className='latest__text'>{item.text}</p>
+                            </li>
+                        ))}
                     </ul>
                 </div>
             </div>
@@ -35,4 +33,4 @@ const Latests = () => {
     )
 }
 
-export default Latests
+export default Latests;
